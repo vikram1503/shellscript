e9f8da5be05c3f03e3b441b7fcd5b55035977038
vikram1503 <vikram15031989@gmail.com>
2024-04-21 12:13:54 +0530

    testing

---
+++ 10functions.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+userid=$(id -u)
+validate(){
+    if [ $1 -ne 0]
+    then
+    echo "$2 installation failed"
+    exit 1
+    else
+    echo "$2 installation success"
+}
+if [ $userid -ne 0 ]
+then
+echo "pls run this script using root access"
+exit 1
+else
+echo "you are super user"
+fi
+dnf install mysql -y
+validate $? "installing mysql"
+ dnf install git -y
+validate $? "installing git"
