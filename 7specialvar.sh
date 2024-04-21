8bdd01fe91fe28b96d82272dc8a6e5b8a7dea21f
vikram1503 <vikram15031989@gmail.com>
2024-04-21 07:36:56 +0530

    testing

--- 7specialvar.sh
+++ 7specialvar.sh
@@ -7,6 +7,9 @@ echo "current directory:$PWD"
 echo "home directory:$HOME"
 echo "user:$USER"
 echo "hostname:$hostname"
-echo "prcoss id:$$"
+echo "process id:$$"
+sleep 30
+echo "process id of last background command:$!"
+
 
 
