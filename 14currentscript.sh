55aa24c48f30ade03a86c5ef39add916d05bcfd1
vikram1503 <vikram15031989@gmail.com>
2024-04-24 14:56:23 +0530

    testing

--- 14currentscript.sh
+++ 14currentscript.sh
@@ -5,5 +5,6 @@ course="aaaaaaa"
 echo "before calling other script:$course"
 echo "process id of current shellscript:$$"
 
-./15otherscript.sh
+#./15otherscript.sh
+source ./15otherscript.sh
 echo "after calling other script:$course"
