2ded1a133cb51b3c0cdd7fea9cc2dd0755f60c3e
vikram1503 <vikram15031989@gmail.com>
2024-04-24 14:42:20 +0530

    testing

--- 14currentscript.sh
+++ 14currentscript.sh
@@ -5,5 +5,5 @@ course="devops from current strip"
 echo "before calling other script:$course"
 echo "process id of current shellscript:$$"
 
-#./15otherscript.sh
+./15otherscript.sh
 echo "after calling other script:$course"
---
+++ 15otherscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+ course="devops from other script"
+
+echo "variable value from other script:$course"
+echo "process id of other script :$$"
